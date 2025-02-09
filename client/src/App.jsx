95891e09bdbc8f1862decd7655abbbd839610337
waibaBikash <kavrelibikash@gmail.com>
2025-02-09 21:33:26 +1100

    create navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, useMatch } from 'react-router-dom'
 import Home from './pages/student/Home'
 import CoursesList from './pages/student/CoursesList'
 import CourseDetials from './pages/student/CourseDetials'
@@ -11,10 +11,15 @@ import Dashboard from './pages/educator/Dashboard'
 import AddCourse from './pages/educator/AddCourse'
 import MyCourses from './pages/educator/MyCourses'
 import StudentsEnrolled from './pages/educator/StudentsEnrolled'
+import Navbar from './components/student/Navbar'
 
 const App = () => {
+
+  const isEducatorRoute = useMatch('/educator/*')
   return (
-    <div>
+    <div className='text-default min-h-screen bg-white'>
+      {!isEducatorRoute && <Navbar/>}
+      
       <Routes>
          <Route path='/' element={<Home/>}/>
          <Route path='/course-list' element={<CoursesList/>} />
--- client/src/components/student/Navbar.jsx
+++ client/src/components/student/Navbar.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import { assets } from '../../assets/assets'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
+
+  const isCourseListPage = location.pathname.includes('/course-list')
   return (
-    <div>
-      <h1>Navbar</h1>
+    <div className={`flex items-center justify-between px-4 sm:px-10 md:px-14 lg:px-36 border-b border-gray-500 py-4 ${isCourseListPage ? 'bg-white' : 'bg-cyan-100/70'}`}>
+       <img src={assets.logo} alt="logo" className='w-28 lg:w-32 cursor-pointer' />
+        <div className='hidden md:flex items-center gap-5 text-gray-500'>
+            <div className='flex items-center gap-5'>
+               <button>Become Educator</button>
+               <Link to="/my-enrollments">My Enrollments</Link>
+            </div>
+             <button className='bg-blue-600 text-white px-5 py-2 rounded-full'>Create Account</button>
+        </div>
+         {/* For Phone Screen */}
+        <div className='md:hidden flex items-center gap-2 sm:gap-5 text-gray-500'>
+             <div>
+             <button>Become Educator</button>
+             <Link to="/my-enrollments">My Enrollments</Link>
+             </div>
+             <button><img src={assets.user_icon} alt="" /></button>
+        </div>
       </div>
   )
 }
