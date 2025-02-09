84a30d95338ec78793410e6725db0a465b5aa587
waibaBikash <kavrelibikash@gmail.com>
2025-02-09 21:00:55 +1100

    initial commit all route working

--- client/src/pages/educator/Educator.jsx
+++ client/src/pages/educator/Educator.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
 
 const Educator = () => {
   return (
     <div>
      <h1> Educator Page</h1>
+       <div>
+          {<Outlet/>}
+       </div>
       </div>
   )
 }
