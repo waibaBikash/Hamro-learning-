fa9e621b45b4a4bdfebcbda4fa36823547378284
waibaBikash <kavrelibikash@gmail.com>
2025-02-11 22:07:33 +1100

    create hero page

--- client/src/pages/student/Home.jsx
+++ client/src/pages/student/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Hero from '../../components/student/Hero'
 
 const Home = () => {
   return (
-    <div>
-      <h1>Home Page</h1>
-      </div>
+    <div className='flex flex-col items-center space-y-7 text-center '>
+      <Hero/>
+    </div>
   )
 }
 
