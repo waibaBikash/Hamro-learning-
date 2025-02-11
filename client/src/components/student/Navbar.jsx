b090002962bdb5ad668c4aa9cbeb655b74cbf1b4
waibaBikash <kavrelibikash@gmail.com>
2025-02-11 21:44:15 +1100

    login functionality

--- client/src/components/student/Navbar.jsx
+++ client/src/components/student/Navbar.jsx
@@ -1,27 +1,43 @@
 import React from 'react'
 import { assets } from '../../assets/assets'
 import { Link } from 'react-router-dom'
+import { useClerk, useUser, UserButton } from '@clerk/clerk-react'
 
 const Navbar = () => {
 
-  const isCourseListPage = location.pathname.includes('/course-list')
+  const isCourseListPage = location.pathname.includes('/course-list');
+
+  const {openSignIn} = useClerk()
+  const {user} = useUser()
   return (
     <div className={`flex items-center justify-between px-4 sm:px-10 md:px-14 lg:px-36 border-b border-gray-500 py-4 ${isCourseListPage ? 'bg-white' : 'bg-cyan-100/70'}`}>
        <img src={assets.logo} alt="logo" className='w-28 lg:w-32 cursor-pointer' />
         <div className='hidden md:flex items-center gap-5 text-gray-500'>
             <div className='flex items-center gap-5'>
-               <button>Become Educator</button>
-               <Link to="/my-enrollments">My Enrollments</Link>
+               { user && <>
+                <button>Become Educator</button>
+               | <Link to="/my-enrollments">My Enrollments</Link>
+               </>
+               }
             </div>
-             <button className='bg-blue-600 text-white px-5 py-2 rounded-full'>Create Account</button>
+             { user ? <UserButton/> :
+              <button onClick={()=> openSignIn()} className='bg-blue-600 text-white px-5 py-2 rounded-full'>Create Account</button>}
         </div>
          {/* For Phone Screen */}
         <div className='md:hidden flex items-center gap-2 sm:gap-5 text-gray-500'>
-             <div>
-             <button>Become Educator</button>
-             <Link to="/my-enrollments">My Enrollments</Link>
+             <div className='flex items-center gap-1 sm:gap-2 max-sm:text-xs'>
+             { user && <>
+              <button>Become Educator</button>
+             | <Link to="/my-enrollments">My Enrollments</Link>
+             </>
+             }
              </div>
-             <button><img src={assets.user_icon} alt="" /></button>
+             {
+               user
+                ? <UserButton/>
+                :  <button><img src={assets.user_icon} alt="" /></button>   
+             }
+             
         </div>
       </div>
   )
