d1d21be39ac940fc7604f7aec1c727c9eace2bae
waibaBikash <kavrelibikash@gmail.com>
2025-02-12 22:26:00 +1100

    changes made

--- client/src/components/student/Navbar.jsx
+++ client/src/components/student/Navbar.jsx
@@ -10,7 +10,8 @@ const Navbar = () => {
   const {openSignIn} = useClerk()
   const {user} = useUser()
   return (
-    <div className={`flex items-center justify-between px-4 sm:px-10 md:px-14 lg:px-36 border-b border-gray-500 py-4 ${isCourseListPage ? 'bg-white' : 'bg-cyan-100/70'}`}>
+    <div 
+    className={`flex items-center justify-between px-4 sm:px-10 md:px-14 lg:px-36 border-b border-gray-500 py-4 ${isCourseListPage ? 'bg-white' : 'bg-cyan-100/70'}`}>
        <img src={assets.logo} alt="logo" className='w-28 lg:w-32 cursor-pointer' />
         <div className='hidden md:flex items-center gap-5 text-gray-500'>
             <div className='flex items-center gap-5'>
@@ -21,7 +22,10 @@ const Navbar = () => {
                }
             </div>
              { user ? <UserButton/> :
-              <button onClick={()=> openSignIn()} className='bg-blue-600 text-white px-5 py-2 rounded-full'>Create Account</button>}
+              <button 
+              onClick={()=> openSignIn()} className='bg-blue-600 text-white px-5 py-2 rounded-full'>
+                Create Account
+              </button>}
         </div>
          {/* For Phone Screen */}
         <div className='md:hidden flex items-center gap-2 sm:gap-5 text-gray-500'>
@@ -43,4 +47,4 @@ const Navbar = () => {
   )
 }
 
-export default Navbar
+export default Navbar;
