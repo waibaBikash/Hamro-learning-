658a010261258916ff51c2d126614d61bec69322
waibaBikash <kavrelibikash@gmail.com>
2025-02-12 22:24:47 +1100

    commit change

--- client/src/components/student/SearchBar.jsx
+++ client/src/components/student/SearchBar.jsx
@@ -18,7 +18,12 @@ const SearchBar = ({data}) => {
            <img 
             src={assets.search_icon} alt="search icon" className='md:w-auto w-10 px-3' 
             />
-           <input onChange={e => setInput(e.target.value)} value={input} type="text" placeholder='Search for couuses' className='w-full h-full outline-none text-gray-500/80' />
+           <input 
+           onChange={e => setInput(e.target.value)}
+           value={input} type="text" 
+           placeholder='Search for couuses'
+           className='w-full h-full outline-none text-gray-500/80'
+            />
            <button 
             type='submit' className='bg-blue-600 rouded text-white md:px-10 px-7 md:py-3 py-2 mx-1'>
               Search
