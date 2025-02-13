5ac4e2dcb9536e24fc59c6e316832ed39bf47d3a
waibaBikash <kavrelibikash@gmail.com>
2025-02-13 21:50:24 +1100

    create course section

--- client/src/pages/student/Home.jsx
+++ client/src/pages/student/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from '../../components/student/Hero'
 import Companies from '../../components/student/ Companies'
+import CoursesSection from '../../components/student/CoursesSection'
 
 const Home = () => {
   return (
     <div className='flex flex-col items-center space-y-7 text-center '>
       <Hero/>
       <Companies/>
+      <CoursesSection/>
     </div>
   )
 }
