74ecdd036cdbc00c863884eed87cecdc1336e69e
waibaBikash <kavrelibikash@gmail.com>
2025-02-13 21:42:52 +1100

    create a companies

--- client/src/components/student/ Companies.jsx
+++ client/src/components/student/ Companies.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import { assets } from '../../assets/assets'
 
 const  Companies = () => {
   return (
-    <div> 
-      <h1>Companies</h1>
-      </div>
+    <div className='pt-16'> 
+      <p className='text-base text-gray-500'>Turested by learners from</p>
+       <div className='flex flex-wrap items-center justify-center gap-6 md:gap-16 md:mt-10 mt-5'>
+         <img src={assets.microsoft_logo} alt="Microsoft" className='w-20 md:w-28' />
+         <img src={assets.walmart_logo} alt="Walmart" className='w-20 md:w-28' />
+         <img src={assets.accenture_logo} alt="Accenture" className='w-20 md:w-28' />
+         <img src={assets.adobe_logo} alt="Adobe" className='w-20 md:w-28' />
+         <img src={assets.paypal_logo} alt="Paypal" className='w-20 md:w-28' />
+       </div>
+    </div>
   )
 }
 
--- client/src/pages/student/Home.jsx
+++ client/src/pages/student/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from '../../components/student/Hero'
+import Companies from '../../components/student/ Companies'
 
 const Home = () => {
   return (
     <div className='flex flex-col items-center space-y-7 text-center '>
       <Hero/>
+      <Companies/>
     </div>
   )
 }
