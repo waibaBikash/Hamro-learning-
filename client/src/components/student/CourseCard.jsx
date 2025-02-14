8706c5164e83a1111e9e04c6ee42b8cd798791b5
waibaBikash <kavrelibikash@gmail.com>
2025-02-14 21:19:13 +1100

    fixed problem

--- client/src/components/student/CourseCard.jsx
+++ client/src/components/student/CourseCard.jsx
@@ -14,7 +14,7 @@ const CourseCard = ({course}) => {
        <div className='p-3 text-left'>
          <h3 className='text-base font-semibold'>{course.courseTitle}</h3>
          <p className='text-gray-500'>{course.educator.name}</p>
-          <div className='felx items-center space-x-2 '> 
+          <div className='flex items-center space-x-2'> 
              <p>4.5</p>
               <div className='flex'>
                 {[...Array(5)].map((_,i)=>(<img className='w-3.5 h-3.5' key={i} src={assets.star} alt=''/>
