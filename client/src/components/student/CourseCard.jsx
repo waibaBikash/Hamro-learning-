3e30a602ddccb8a98dccedf253b13f599d210bbe
waibaBikash <kavrelibikash@gmail.com>
2025-02-15 21:20:56 +1100

    commit change

--- client/src/components/student/CourseCard.jsx
+++ client/src/components/student/CourseCard.jsx
@@ -20,7 +20,7 @@ const CourseCard = ({course}) => {
                 {[...Array(5)].map((_,i)=>(<img className='w-3.5 h-3.5' key={i} src={i < Math.floor(calculateRating(course)) ? assets.star : assets.star_blank} alt=''/>
               ))}
               </div>
-              <p className='text-gray-500'>22</p>
+              <p className='text-gray-500'>{course.courseRatings.length}</p>
           </div>
            <p className='text-base font-semibold text-gray-800'>{currency}{(course.coursePrice - course.discount * course.coursePrice / 100).toFixed(2)}</p>
        </div>
