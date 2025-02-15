94d532748cf6d259c3e10f1c1112d0ff01a9a058
waibaBikash <kavrelibikash@gmail.com>
2025-02-15 21:56:14 +1100

    create testimonials

--- client/src/pages/student/Home.jsx
+++ client/src/pages/student/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Hero from '../../components/student/Hero'
 import Companies from '../../components/student/ Companies'
 import CoursesSection from '../../components/student/CoursesSection'
+import TestimonialsSection from '../../components/student/TestimonialsSection'
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <Hero/>
       <Companies/>
       <CoursesSection/>
+      <TestimonialsSection/>
     </div>
   )
 }
