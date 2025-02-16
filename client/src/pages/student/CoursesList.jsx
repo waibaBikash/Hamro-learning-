47642350c1c781891e3b2665c1650c17e738d078
waibaBikash <kavrelibikash@gmail.com>
2025-02-16 21:12:05 +1100

    complete course list

--- client/src/pages/student/CoursesList.jsx
+++ client/src/pages/student/CoursesList.jsx
@@ -1,18 +1,28 @@
 import React, { useContext } from 'react'
 import { AppContext } from '../../context/AppContext'
+import SearchBar from '../../components/student/SearchBar'
+import { useParams } from 'react-router-dom'
+import CourseCard from '../../components/student/CourseCard'
 
 const CoursesList = () => {
 
-  const {navigate} = useContext(AppContext)
+  const {navigate, allCourses} = useContext(AppContext)
+  const {input} = useParams()
   return (
     <>
-      <div>
-         <div>
-          <h1>Course List</h1>
+      <div className='relative md:px-36 px-8 pt-20 text-left'>
+         <div className='flex md:flex-row gap-6 items-start justify-between w-full'>
+           <div>
+           <h1>Course List</h1>
           <p className='text-gray-500'> 
             <span className='text-blue-600 cursor-pointer' onClick={()=> navigate('/')}>Home</span> / <span>Course List</span>
             </p>
+           </div>
+             <SearchBar data={input}/>
          </div>
+          <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 my-16 gap-3 px-2 md:p-0'>
+             {allCourses.map((course, index)=> <CourseCard key={index} course={course}/>)}
+          </div>
       </div>
     </>
   )
