1b96054b3f2200a2aa8671269b7be5a6c8078e7d
waibaBikash <kavrelibikash@gmail.com>
2025-02-17 22:13:29 +1100

    create course details

--- client/src/pages/student/CourseDetials.jsx
+++ client/src/pages/student/CourseDetials.jsx
@@ -1,11 +1,42 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { AppContext } from '../../context/AppContext'
+import Loading from '../../components/student/Loading'
 
 const CourseDetials = () => {
-  return (
-    <div>
-      <h1>CourseDetials Page</h1>
+
+  const {id} = useParams()
+
+  const [courseData, setCourseData] = useState(null)
+
+  const {allCourses} = useContext(AppContext)
+
+  const fetchCourseData = async ()=>{
+   const findCourse =  allCourses.find(course => course._id === id)
+   setCourseData(findCourse)
+  }
+
+  useEffect(()=>{
+   fetchCourseData()
+  },[])
+  return courseData ? (
+    <>
+    <div className='flex md:flex-row flex-col-reverse gap-10 relative items-start justify-between md:px-36 px-8 md:pt-30 pt-20 text-left'>
+      
+      <div className='absolute top-0 left-0 w-full h-section-height -z-1 bg-gradient-to-b from-cyan-100/70 '></div>
+
+      {/* Left Column */}
+      <div>
+         <h1>{courseData.courseTitle}</h1>
+         <p dangerouslySetInnerHTML={{_html: courseData.courseDescription.slice(0,200)}}></p>
       </div>
-  )
+
+      {/* Right Column */}
+      <div></div>
+
+    </div>
+    </>
+  ) : <Loading/>
 }
 
 export default CourseDetials
