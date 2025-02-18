e7bed50f084b570d4be1b9b70a7f15196a2b5cc2
waibaBikash <kavrelibikash@gmail.com>
2025-02-18 21:10:00 +1100

    create a cousedetails

--- client/src/pages/student/CourseDetials.jsx
+++ client/src/pages/student/CourseDetials.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { AppContext } from '../../context/AppContext'
 import Loading from '../../components/student/Loading'
+import { assets } from '../../assets/assets'
 
 const CourseDetials = () => {
 
@@ -9,7 +10,7 @@ const CourseDetials = () => {
 
   const [courseData, setCourseData] = useState(null)
 
-  const {allCourses} = useContext(AppContext)
+  const {allCourses, calculateRating} = useContext(AppContext)
 
   const fetchCourseData = async ()=>{
    const findCourse =  allCourses.find(course => course._id === id)
@@ -26,10 +27,22 @@ const CourseDetials = () => {
       <div className='absolute top-0 left-0 w-full h-section-height -z-1 bg-gradient-to-b from-cyan-100/70 '></div>
 
       {/* Left Column */}
-      <div>
-         <h1>{courseData.courseTitle}</h1>
+      <div className='max-w-xl z-10 text-gray-500'>
+         <h1 className='md:text-course-details-heading-large text-course-details-heading-small font-semibold text-gray-800'>{courseData.courseTitle}</h1>
          {/*  use double undescore */}
-         <p dangerouslySetInnerHTML={{__html: courseData.courseDescription.slice(0,200)}}></p>
+         <p className='pt-4 md:text-base text-sm'
+         dangerouslySetInnerHTML={{__html: courseData.courseDescription.slice(0,200)}}></p>
+
+         {/* review and rating */}
+
+         <div className='flex items-center space-x-2'> 
+                      <p>{calculateRating(courseData)}</p>
+                       <div className='flex'>
+                         {[...Array(5)].map((_,i)=>(<img className='w-3.5 h-3.5' key={i} src={i < Math.floor(calculateRating(courseData)) ? assets.star : assets.star_blank} alt=''/>
+                       ))}
+                       </div>
+                       <p className='text-gray-500'>{courseData.courseRatings.length}</p>
+                   </div>
       </div>
 
       {/* Right Column */}
