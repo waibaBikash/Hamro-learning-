30296e9da231fc2324bd6eab00d9f86151be455b
waibaBikash <kavrelibikash@gmail.com>
2025-02-27 20:32:27 +1100

    commit change

--- client/src/pages/student/CourseDetials.jsx
+++ client/src/pages/student/CourseDetials.jsx
@@ -62,7 +62,11 @@ const CourseDetials = () => {
          <div className='flex items-center space-x-2 pt-3 pb-1 text-sm'> 
                       <p>{calculateRating(courseData)}</p>
                        <div className='flex'>
-                         {[...Array(5)].map((_,i)=>(<img className='w-3.5 h-3.5' key={i} src={i < Math.floor(calculateRating(courseData)) ? assets.star : assets.star_blank} alt=''/>
+                         {[...Array(5)].map((_,i)=>(<img 
+                         className='w-3.5 h-3.5' key={i} 
+                         src={i < Math.floor(calculateRating(courseData))
+                         ? assets.star
+                         : assets.star_blank} alt=''/>
                        ))}
                        </div>
                        <p className='text-blue-600'>({courseData.courseRatings.length}
