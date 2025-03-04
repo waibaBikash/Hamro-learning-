18ea2695a08e0015a5febcd8296f866ca9529236
waibaBikash <kavrelibikash@gmail.com>
2025-03-04 20:47:56 +1100

    changes made

--- client/src/pages/student/MyEnrollments.jsx
+++ client/src/pages/student/MyEnrollments.jsx
@@ -51,10 +51,13 @@ const MyEnrollments = () => {
                      {calculateCourseDuration(course)}
                    </td>
                    <td className='px-4 py-3 max-sm:hidden'>
-                    {progressArray[index] && `${progressArray[index].lectureCompleted} / ${progressArray[index].totalLectures} `} <span>Lectures</span>
+                    {progressArray[index] && `${progressArray[index].lectureCompleted} / ${progressArray[index].totalLectures} `}
+                     <span>Lectures</span>
                    </td>
                    <td className='px-4 py-3 max-sm:hidden'>
-                     <button onClick={()=> navigate('/player/' + course._id)} className='px-3 sm:px-5 py-1.5 sm:py-2 bg-blue-600 max-sm:text-xs text-white'>
+                     <button
+                       onClick={()=> navigate('/player/' + course._id)} 
+                       className='px-3 sm:px-5 py-1.5 sm:py-2 bg-blue-600 max-sm:text-xs text-white'>
                       {progressArray[index] && progressArray[index].lectureCompleted / progressArray[index].totalLectures === 1
                       ? 'Completed'
                     : 'On Going' }
