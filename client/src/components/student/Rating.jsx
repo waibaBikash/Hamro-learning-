faebf63e8a479de6d1fc3b36de106a80492935df
waibaBikash <kavrelibikash@gmail.com>
2025-03-05 21:15:06 +1100

    commit chane

--- client/src/components/student/Rating.jsx
+++ client/src/components/student/Rating.jsx
@@ -19,7 +19,9 @@ const Rating = ({initialRating, onRate}) => {
        {Array.from({length: 5}, (_, index)=>{
          const starValue = index + 1;
          return (
-          <span key={index} className={`text-xl sm:text-2xl cursor-pointer transition-colors ${starValue <= rating ? 'text-yellow-500' : 'text-gray-400'}`}>
+          <span
+          onClick={()=> handleRating(starValue)}
+           key={index} className={`text-xl sm:text-2xl cursor-pointer transition-colors ${starValue <= rating ? 'text-yellow-500' : 'text-gray-400'}`}>
             &#9733;
           </span>
          )
--- client/src/pages/student/Player.jsx
+++ client/src/pages/student/Player.jsx
@@ -5,6 +5,7 @@ import { assets } from '../../assets/assets';
 import humanizeDuration from 'humanize-duration';
 import YouTube from 'react-youtube';
 import Footer from '../../components/student/Footer';
+import Rating from '../../components/student/Rating';
 
 const Player = () => {
 
@@ -84,6 +85,7 @@ const Player = () => {
               </div>
                <div className='flex items-center gap-2 py-3 mt-10'>
                  <h1 className='text-xl font-bold'>Rate this Course:</h1>
+                 <Rating initialRating={0}/>
                </div>
       </div>
 
