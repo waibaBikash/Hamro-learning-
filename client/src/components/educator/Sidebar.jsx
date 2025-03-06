8a33baaba24bbec2dc734bc70d8b63ad160664e5
waibaBikash <kavrelibikash@gmail.com>
2025-03-06 21:24:35 +1100

    cteater sidebar

--- client/src/components/educator/Sidebar.jsx
+++ client/src/components/educator/Sidebar.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { assets } from '../../assets/assets';
+import { AppContext } from '../../context/AppContext';
+import { NavLink } from 'react-router-dom';
 
 const Sidebar = () => {
-  return (
-    <div>
-      <h1>Edu Sidebar</h1>
-      </div>
+
+  const { isEducator } = useContext(AppContext)
+
+  const menuItems = [
+    {name: 'Dashboard', path: '/educator', icon: assets.home_icon},
+    {name: 'Add Courses', path: '/educator/add-course', icon: assets.add_icon},
+    {name: 'My Courses', path: '/educator/my-courses', icon: assets.my_course_icon},
+    {name: 'Student Enrolled', path: '/educator/student-enrolled', icon: assets.person_tick_icon
+    },
+  ];
+  return isEducator && (
+    <div className='md:w-64 w-1/6 border-r min-h-screen text-base border-gray-500'>
+      {menuItems.map((item)=>{
+        <NavLink>
+           <img src={item.icon} alt=""  className='w-6 h-6'/>
+           <p className='md:block hidden text-center'>{item.name}</p>
+        </NavLink>
+      })}
+    </div>
   )
 }
 
