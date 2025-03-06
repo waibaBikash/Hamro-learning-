f6abd7c556b66c92b87aac16c2ec88401a3a43ac
waibaBikash <kavrelibikash@gmail.com>
2025-03-06 21:55:00 +1100

    cteate footer

--- client/src/components/educator/Footer.jsx
+++ client/src/components/educator/Footer.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import { assets } from '../../assets/assets'
 
 const Footer = () => {
   return (
-    <div>
-      <h1>Edu Footer</h1>
-      </div>
+    <footer className="flex md:flex-row flex-col-reverse items-center justify-between text-left w-full px-8 border-t">
+       <div className='flex items-center gap-4'>
+        <img src={assets.logo} alt="logo" className='hidden md:block w-20' />
+         <div className='hidden md:block h-7 w-px bg-gray-500/60'></div>
+         <p>Copyright 2025 © Hamro-Learning. All Right Reserved.</p>
+       </div>
+        <div className='flex items-center gap-3 max-md:mt-4'>
+          <a href="#"><img src={assets.facebook_icon} alt="facebook_icon" /></a>
+          <a href="#"><img src={assets.twitter_icon} alt="twitter_icon" /></a>
+          <a href="#"><img src={assets.instagram_icon} alt="instagram_icon" /></a>
+        </div>
+
+    </footer>
   )
 }
 
--- client/src/pages/educator/Educator.jsx
+++ client/src/pages/educator/Educator.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Navbar from '../../components/educator/Navbar'
 import Sidebar from '../../components/educator/Sidebar'
+import Footer from '../../components/educator/Footer'
 
 const Educator = () => {
   return (
@@ -13,6 +14,7 @@ const Educator = () => {
           {<Outlet/>}
           </div>
        </div>
+       <Footer/>
       </div>
   )
 }
