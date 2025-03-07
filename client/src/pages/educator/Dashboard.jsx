f48c620a4e7d5b2694659569aedf2f9ebf35bc97
waibaBikash <kavrelibikash@gmail.com>
2025-03-07 21:22:25 +1100

    create table data

--- client/src/pages/educator/Dashboard.jsx
+++ client/src/pages/educator/Dashboard.jsx
@@ -53,6 +53,36 @@ const Dashboard = () => {
 
               </div>
           </div>
+
+            <div>
+              <h2 className='pb-4 text-lg font-medium'>Latest Enrolments</h2>
+               <div className='flex flex-col items-center max-w-4xl w-full overflow-hidden rounded-md bg-white border border-gray-500/20'>
+                   <table className='table-fixed md:table-auto w-full overflow-hidden'>
+                      <thead className='text-gray-900 border-b bg-orange-500/20 text-sm text-left'>
+                          <tr>
+                             <th className='px-4 py-3 font-semibold text-center hidden sm:table-cell'>#</th>
+                             <th className='px-4 py-3 font-semibold'>Student Name</th>
+                             <th className='px-4 py-3 font-semibold'>Course Title</th>
+                          </tr>
+                      </thead>
+                       
+                       <tbody className='text-sm text-gray-500'>
+                          {dashboardData.enrolledStudentsData.map((item, index) => (
+                             <tr key={index} className='border-b border-gray-500/20'>
+                               <td className='px-4 py-3 text-center hidden sm:table-cell'>
+                                 {index + 1}
+                               </td>
+                               <td className='md:px-4 px-2 py-3 flex text-center space-x-3'>
+                                <img src={item.student.inmageUrl} alt="Profile" className='w-9 h-9 rounded-full' />
+                                <span>{item.student.name}</span>
+                               </td>
+                               <td className='px-4 py-3 truncate'>{item.courseTitle}</td>
+                             </tr>
+                          ))}
+                       </tbody>
+                   </table>
+               </div>
+            </div>
        </div>
     
      </div>
