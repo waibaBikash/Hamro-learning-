5311fb371c2ce632e4c843d86845ea3a3c4be65a
waibaBikash <kavrelibikash@gmail.com>
2025-03-07 20:53:24 +1100

    save change

--- client/src/pages/educator/Dashboard.jsx
+++ client/src/pages/educator/Dashboard.jsx
@@ -18,21 +18,21 @@ const Dashboard = () => {
   },[])
 
   return dashboardData ? (
-    <div className='min-h-screen flex flex-col items-start justify-between gap-8 md:pb-0 pt-8 pb-0'>
+    <div className='min-h-screen flex flex-col items-start justify-between gap-8 md:p-8 md:pb-0 p-4 pt-8 pb-0'>
        
        <div className='space-y-5'>
           <div className='flex flex-wrap gap-5 items-center '>
-              <div className='flex items-center gap-3 shadow-card  border border-blue-500 p-4 w-5/6 rounded-md'>
+              <div className='flex items-center gap-3 shadow-card  border border-blue-500 p-4 w-56 rounded-md'>
               <img src={assets.patients_icon} alt="patient_icon" />
-               <div>
+                 <div>
                  <p className='text-2xl font-medium text-gray-600'>
                   {dashboardData.enrolledStudentsData.length}
                  </p>
                  <p className='text-base text-gray-500'>Total Enrollements</p>
-               </div>
+                 </div>
 
               </div>
-              <div className='flex items-center gap-3 shadow-card  border border-blue-500 p-4 w-5/6 rounded-md'>
+              <div className='flex items-center gap-3 shadow-card  border border-blue-500 p-4 w-56 rounded-md'>
               <img src={assets.appointments_icon} alt="patient_icon" />
                <div>
                  <p className='text-2xl font-medium text-gray-600'>
@@ -42,11 +42,11 @@ const Dashboard = () => {
                </div>
 
               </div>
-              <div className='flex items-center gap-3 shadow-card  border border-blue-500 p-4 w-5/6 rounded-md'>
+              <div className='flex items-center gap-3 shadow-card  border border-blue-500 p-4 w-56 rounded-md'>
               <img src={assets.earning_icon} alt="patient_icon" />
                <div>
                  <p className='text-2xl font-medium text-gray-600'>
-                  {dashboardData.totalEarning}
+                  {dashboardData.totalEarnings}
                  </p>
                  <p className='text-base text-gray-500'>Total Earnings</p>
                </div>
