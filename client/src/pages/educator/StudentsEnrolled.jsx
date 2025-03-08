36c2f52f7a6697c374ed98fc48bf53b838effd3a
waibaBikash <kavrelibikash@gmail.com>
2025-03-08 21:37:46 +1100

    complete enrolled page

--- client/src/pages/educator/StudentsEnrolled.jsx
+++ client/src/pages/educator/StudentsEnrolled.jsx
@@ -25,6 +25,21 @@ const StudentsEnrolled = () => {
                  <th className='px-4 py-3 font-semibold'>Date</th>
                </tr>
              </thead>
+              <tbody>
+                {enrolledStudents.map((item, index) => (
+                  <tr key={index} className='border-b border-gray-500/20'>
+                    <td className='px-4 py-3 text-center hidden sm:table-cell'>{index + 1}</td>
+                    <td className='md:px-4 px-2 py-3 flex items-center space-x-3'>
+                       <img src={item.student.imageUrl} alt="" 
+                       className='w-9 h-9 rounded-full'/>
+                       <span className='truncate'>{item.student.name}</span>
+                    </td>
+
+                    <td className='px-4 py-3 truncate'>{item.courseTitle}</td>
+                    <td className='px-4 py-3 hidden sm:table-cell'>{new Date(item.purchaseDate).toLocaleDateString()}</td>
+                  </tr>
+                ))}
+              </tbody>
           </table>
        </div>
       
