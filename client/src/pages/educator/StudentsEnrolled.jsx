48790916c18414232c78c81826885dc195118ec8
waibaBikash <kavrelibikash@gmail.com>
2025-03-08 21:22:58 +1100

    create student enroll table

--- client/src/pages/educator/StudentsEnrolled.jsx
+++ client/src/pages/educator/StudentsEnrolled.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { dummyStudentEnrolled } from '../../assets/assets'
+import Loading from '../../components/student/Loading'
 
 const StudentsEnrolled = () => {
-  return (
-    <div>
-      <h1>Students Enrolled Page</h1>
-      </div>
-  )
+
+  const [enrolledStudents, setEnrolledStudents] = useState(null)
+
+  const fetchEnrolledstudents = async () => {
+     setEnrolledStudents(dummyStudentEnrolled)
+  }
+
+  useEffect(()=>{
+   fetchEnrolledstudents()
+  },[])
+  return enrolledStudents ? (
+    <div className='min-h-screen flex flex-col items-start justify-between md:p-8 md:pb-0 pt-8 pb-0'>
+       <div className='flex flex-col items-center max-w-4xl w-full overflow-hidden rounded-md bg-white border border-gray-500/20'>
+          <table className='table-fixed md:table-auto w-full overflow-hidden pb-4'>
+             <thead className='text-gray-900 border-b border-gray-500/20 text-sm text-left'>
+               <tr>
+                 <th className='px-4 py-3 font-semibold text-center hidden sm:table-cell'>#</th>
+                 <th className='px-4 py-3 font-semibold'>Student Name</th>
+                 <th className='px-4 py-3 font-semibold'>Course Title</th>
+                 <th className='px-4 py-3 font-semibold'>Date</th>
+               </tr>
+             </thead>
+          </table>
+       </div>
+      
+   </div>
+  ) : <Loading/>
 }
 
 export default StudentsEnrolled
