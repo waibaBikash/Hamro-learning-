fedf21c2e1bff759497341008482f3e2577e11ee
waibaBikash <kavrelibikash@gmail.com>
2025-03-12 23:07:11 +1100

    commit change

--- client/src/pages/student/CoursesList.jsx
+++ client/src/pages/student/CoursesList.jsx
@@ -31,7 +31,10 @@ const CoursesList = () => {
       <div className='relative md:px-36 px-8 pt-20 text-left'>
          <div className='flex md:flex-row flex-col gap-6 items-start justify-between w-full'>
            <div>
-           <h1 className='text-4xl font-semibold text-gray-800'>Course List</h1>
+           <h1 
+            className='text-4xl font-semibold text-gray-800'>
+              Course List
+              </h1>
           <p className='text-gray-500'> 
             <span 
             className='text-blue-600 cursor-pointer'
